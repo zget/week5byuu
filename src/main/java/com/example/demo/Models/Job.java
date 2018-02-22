a4221e342a5ccce8eefa792a1c4107fd46eae302
zget <dgetahun84@gmail.com>
2018-02-22 14:04:40 -0500

    second commit

--- src/main/java/com/example/demo/Models/Job.java
+++ src/main/java/com/example/demo/Models/Job.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.ArrayList;
 
 @Entity
 public class Job {
@@ -21,4 +22,46 @@ public class Job {
 
     ArrayList<String> desiredSkill;
 
+    public Job() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getDescrption() {
+        return descrption;
+    }
+
+    public void setDescrption(String descrption) {
+        this.descrption = descrption;
+    }
+
+    public ArrayList<String> getDesiredSkill() {
+        return desiredSkill;
+    }
+
+    public void setDesiredSkill(ArrayList<String> desiredSkill) {
+        this.desiredSkill = desiredSkill;
+    }
 }
